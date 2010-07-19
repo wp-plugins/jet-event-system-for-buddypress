6c0f7bfc32fe9237d69c10d7cbbad7b36b2298a0
milordk <milordk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-19 17:42:08 +0000

    git-svn-id: https://plugins.svn.wordpress.org/jet-event-system-for-buddypress/trunk@264638 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jet-event-system.php
+++ jet-event-system.php
@@ -3,14 +3,14 @@
 Plugin Name: Jet Event System for BuddyPress
 Plugin URI: http://milordk.ru/r-lichnoe/opyt/cms/jet-event-system-for-buddypress-sistema-sobytij-dlya-vashej-socialnoj-seti.html
 Description: System events for your social network. Ability to attract members of the network to the ongoing activities.
-Version: 1.0.6
+Version: 1.0.8
 Author: Jettochkin
 Author URI: http://milordk.ru/
 Site Wide Only: true
 Network: true
 */
 
-define('Jet Events System', '1.0.7');
+define('Jet Events System', '1.0.8');
 define ('JES_EVENTS_DB_VERSION', '1.0');
 
 /* Define the slug for the component */
